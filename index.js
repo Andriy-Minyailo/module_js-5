9929708f496059d6d6ed510b8c5b5f7a83e33699
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-02-24 23:31:43 +0200

    add task_2

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 // task_1
-const pizzaPalace = {
-  pizzas: ["Supercheese", "Smoked", "Four meats"],
+// const pizzaPalace = {
+//   pizzas: ["Supercheese", "Smoked", "Four meats"],
+//   // Change code below this line
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+//   // Change code above this line
+// };
+
+// task_2
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
   // Change code below this line
-  checkPizza(pizzaName) {
-    return this.pizzas.includes(pizzaName);
+  getBalance() {
+    return this.balance;
   },
-  order(pizzaName) {
-    const isPizzaAvailable = this.checkPizza(pizzaName);
-
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
-
-    return `Order accepted, preparing «${pizzaName}» pizza`;
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
   },
   // Change code above this line
 };
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
