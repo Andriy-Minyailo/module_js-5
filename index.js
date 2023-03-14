503b6a33efe5446e7ef960672d42211568c2a34a
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-03-14 23:36:34 +0200

    add task_20

--- index.js
+++ index.js
@@ -405,6 +405,45 @@
 // }
 
 // task_19
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+// constructor ({email, accessLevel}) {
+//   super(email);
+//   this.accessLevel = accessLevel;
+// }
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// task_20
 class User {
   email;
 
@@ -420,7 +459,6 @@ class User {
     this.email = newEmail;
   }
 }
-
 class Admin extends User {
   // Change code below this line
 
@@ -428,12 +466,24 @@ class Admin extends User {
     BASIC: "basic",
     SUPERUSER: "superuser",
   };
-constructor ({email, accessLevel}) {
-  super(email);
-  this.accessLevel = accessLevel;
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+    this.blacklistedEmails = [];
+  }
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+isBlacklisted(email) {
+  return this.blacklistedEmails.includes(email);
 }
   // Change code above this line
+
+  
 }
+
+
 
 const mango = new Admin({
   email: "mango@mail.com",
@@ -442,3 +492,8 @@ const mango = new Admin({
 
 console.log(mango.email); // "mango@mail.com"
 console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
